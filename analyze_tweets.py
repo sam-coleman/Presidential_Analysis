cb4463ef80ffd808b261969a62f2ee76bc44a5db
sam-coleman <scoleman@olin.edu>
2020-03-06 00:50:21 -0500

    Encapsulated into funcitons

--- analyze_tweets.py
+++ analyze_tweets.py
@@ -25,31 +25,46 @@ def get_freq_dict(user_list, file_name):
     for word in user_list:
         if word not in ignore_words:
             d_freq[word] = d_freq.get(word,0) + 1
-
-    d_sorted = sorted(d_freq.items(), key=lambda kv: kv[1], reverse=True)
-
-    #for fine tuning, delete all this later
-    top = d_sorted[0:50]
-    top_words = []
-    for pair in top:
-        for key in pair:
-            top_words.append(key)
-
-    f = open(file_name, 'w')
-    f.write(str(top_words))
-    f.close()
-    #return top_words
     return d_freq
 
 BidenDict = get_freq_dict(BidenWords, 'BidenDict.txt')
 TrumpDict = get_freq_dict(TrumpWords, 'TrumpDict.txt')
 BernieDict = get_freq_dict(BernieWords, 'BernieDict.txt')
 
-def get_top_n(n, dict):
+def get_top_n(n, dict, file_name):
 
-def create_word_clouds(dict)
-wc = WordCloud(background_color="white",width=3000,height=3000, max_words=30,relative_scaling=0.5,normalize_plurals=False).generate_from_frequencies(TrumpDict)
-plt.imshow(wc, interpolation='bilinear')
-plt.axis("off")
-plt.title('Trump Top Words')
-plt.savefig('deltePic')
+    d_sorted = sorted(dict.items(), key=lambda kv: kv[1], reverse=True)
+    top = d_sorted[0:n]
+    top_words = []
+    for pair in top:
+        for key in pair:
+            top_words.append(key)
+            
+    index = 0
+    while index < len(top_words):
+        if isinstance(top_words[index], int):
+            del top_words[index]
+        else:
+            index += 1
+
+    #Save to file
+    f = open(file_name, 'w')
+    f.write(str(top_words))
+    f.close()
+    return top_words
+
+biden_top = get_top_n(50, BidenDict, 'BidenTopWords.txt')
+trump_top = get_top_n(50, TrumpDict, 'TrumpTopWords.txt')
+bernie_top = get_top_n(50, BernieDict, 'BernieTopWords.txt')
+
+def create_word_cloud(dict, file_name, title):
+
+    wc = WordCloud(background_color="white",width=3000,height=3000, max_words=30,relative_scaling=0.5,normalize_plurals=False).generate_from_frequencies(dict)
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis("off")
+    plt.title(title)
+    plt.savefig(file_name)
+
+trump_cloud = create_word_cloud(TrumpDict, 'TrumpWordCloud', 'Trump Top Words Visualization')
+bernie_cloud = create_word_cloud(BernieDict, 'BernieWordCloud', 'Bernie Top Words Visualization')
+biden_cloud = create_word_cloud(BidenDict, 'BidenWordCloud', 'Biden Top Words Visualization')
