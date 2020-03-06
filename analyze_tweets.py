47b9ffaac4c023e0cb136471cfd5c8153d276ed7
sam-coleman <scoleman@olin.edu>
2020-03-06 17:39:07 -0500

    Fully working, added some docstrings

--- analyze_tweets.py
+++ analyze_tweets.py
@@ -5,13 +5,14 @@ MP 3: Text Mining
 @author: Sam Coleman
 """
 
-from get_tweets import TrumpWords, BernieWords, BidenWords
-#from get_tweets import BernieWords
+#from get_tweets import TrumpWords, BernieWords, BidenWords
+from get_tweets import pull_all_tweets
 from PIL import Image
 from wordcloud import WordCloud
 from matplotlib import pyplot as plt
 
-def get_freq_dict(user_list, del_file):
+
+def get_freq_dict(user_list):
     """
     Get dictionry of top words and their respective frequencies.
 
@@ -20,23 +21,15 @@ def get_freq_dict(user_list, del_file):
     d_freq = dict()
 
     #meaningless words to not include
-    ignore_words = ['the', 'be', 'to', 'of', 'and', 'a', 'in', 'that']
-    ignore_words += ['it', 'for', 'on', 'by', 'from', 'this', 'is']
-    ignore_words += ['—', 'are', 'as', 'at', 'an']
+    ignore_words = ['the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'it',
+    'for', 'on', 'by', 'from', 'this', 'is', 'it', 'for', 'on', 'by', 'from',
+    'this', 'is', '—', 'are', 'as', 'at', 'an', 'amp', 'if', 'has', 'was']
 
     for word in user_list:
         if word not in ignore_words:
             d_freq[word] = d_freq.get(word,0) + 1
 
-    f = open(del_file, 'w')
-    f.write(str(d_freq))
-    f.close()
-
     return d_freq
-
-BidenDict = get_freq_dict(BidenWords, 'BidenAllDelete.txt')
-TrumpDict = get_freq_dict(TrumpWords, 'TrumpAllDelete.txt')
-BernieDict = get_freq_dict(BernieWords, 'BernieAllDelete.txt')
 
 def get_top_n(n, dict, file_name):
 
@@ -60,18 +53,22 @@ def get_top_n(n, dict, file_name):
     f.close()
     return top_words
 
-biden_top = get_top_n(50, BidenDict, 'BidenTopWords.txt')
-trump_top = get_top_n(50, TrumpDict, 'TrumpTopWords.txt')
-bernie_top = get_top_n(50, BernieDict, 'BernieTopWords.txt')
+def create_word_cloud(dict, file_name, title):
 
-# def create_word_cloud(dict, file_name, title):
-#
-#     wc = WordCloud(background_color="white",width=3000,height=3000, max_words=30,relative_scaling=0.5,normalize_plurals=False).generate_from_frequencies(dict)
-#     plt.imshow(wc, interpolation='bilinear')
-#     plt.axis("off")
-#     plt.title(title)
-#     plt.savefig(file_name)
-#
-# trump_cloud = create_word_cloud(TrumpDict, 'TrumpWordCloud', 'Trump Top Words Visualization')
-# bernie_cloud = create_word_cloud(BernieDict, 'BernieWordCloud', 'Bernie Top Words Visualization')
-# biden_cloud = create_word_cloud(BidenDict, 'BidenWordCloud', 'Biden Top Words Visualization')
+    wc = WordCloud(background_color="white",width=3000,height=3000, max_words=30,relative_scaling=0.5,normalize_plurals=False).generate_from_frequencies(dict)
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis("off")
+    plt.title(title)
+    plt.savefig(file_name)
+
+if __name__ == '__main__':
+    TrumpWords, BernieWords, BidenWords = pull_all_tweets()
+    BidenDict = get_freq_dict(BidenWords)
+    TrumpDict = get_freq_dict(TrumpWords)
+    BernieDict = get_freq_dict(BernieWords)
+    biden_top = get_top_n(50, BidenDict, 'BidenTopWords.txt')
+    trump_top = get_top_n(50, TrumpDict, 'TrumpTopWords.txt')
+    bernie_top = get_top_n(50, BernieDict, 'BernieTopWords.txt')
+    trump_cloud = create_word_cloud(TrumpDict, 'TrumpWordCloud1', 'Trump Top Words Visualization')
+    bernie_cloud = create_word_cloud(BernieDict, 'BernieWordCloud1', 'Bernie Top Words Visualization')
+    biden_cloud = create_word_cloud(BidenDict, 'BidenWordCloud1', 'Biden Top Words Visualization')
--- get_tweets.py
+++ get_tweets.py
@@ -17,12 +17,15 @@ tweets = []
 
 def pull_tweets(screen_name, file_name):
     """
-    Get tweets for user and save into a txt file
+    Get tweets for user and save into a pickled file. The list is of all lowercase words (exlcuding hyperlinks)
+
+    screen_name: twitter user's handle
+    file_name: name of file to save tweets to
     """
     user_tweets = []
     user_lines = []
     user_words = []
-    for status in tweepy.Cursor(api.user_timeline, screen_name=screen_name, tweet_mode="extended", include_rts= 0, count = 199).items():
+    for status in tweepy.Cursor(api.user_timeline, screen_name=screen_name, tweet_mode="extended", include_rts= 0).items():
         user_tweets.append(status.full_text)
 
     for line in user_tweets:
@@ -36,7 +39,7 @@ def pull_tweets(screen_name, file_name):
             if not (word[0:4] == "http"):
                 user_words.append(word.strip(string.punctuation))
 
-    #Save to file
+    #Save list of words to pickled file
     f = open(file_name, 'wb')
     pickle.dump(user_words, f)
     f.close()
@@ -44,6 +47,9 @@ def pull_tweets(screen_name, file_name):
 def get_user_tweets(screen_name, file_name):
     """
     Get tweets for the user if file doesn't exist, if it does unpickle it
+
+    screen_name: user's twitter handle
+    file_name: Name of pickled file
     """
     file = Path(file_name)
     if file.is_file():
@@ -52,48 +58,17 @@ def get_user_tweets(screen_name, file_name):
         return reloaded_words
     else:
         pull_tweets(screen_name, file_name)
-        #get_user_tweets(screen_name, file_name)
 
-TrumpWords = get_user_tweets('@realDonaldTrump', 'TrumpTweets.pickle')
-BernieWords = get_user_tweets('@BernieSanders', 'BernieTweets.pickle')
-BidenWords = get_user_tweets('@JoeBiden', 'BidenTweets.pickle')
+def pull_all_tweets():
+    """
+    Pull tweets for Trump, Bernie, Biden
+    """
+    TrumpWords = get_user_tweets('@realdonaldtrump', 'TrumpTweets.pickle')
+    BernieWords = get_user_tweets('@BernieSanders', 'BernieTweets.pickle')
+    BidenWords = get_user_tweets('@JoeBiden', 'BidenTweets.pickle')
+    return TrumpWords, BernieWords, BidenWords
 
-# def get_trump_tweets():
-#     """
-#     Get Trump's Tweets if file doesn't exist, if it does unpickle it
-#     """
-#     file = Path('TrumpTweets.pickle')
-#     if file.is_file():
-#         input_file = open('TrumpTweets.pickle', 'rb')
-#         reloaded_words = pickle.load(input_file)
-#         return reloaded_words
-#     else:
-#         pull_tweets('@realDonaldTrump', 'TrumpTweets.pickle')
-#
-# def get_bernie_tweets():
-#     """
-#     Get Bernie's Tweets if file doesn't exist, if it does unpickle it
-#     """
-#     file = Path('BernieTweets.pickle')
-#     if file.is_file():
-#         input_file = open('BernieTweets.pickle', 'rb')
-#         reloaded_words = pickle.load(input_file)
-#         return reloaded_words
-#     else:
-#         get_tweets('@BernieSanders', 'BernieTweets.pickle')
-#
-# def get_biden_tweets():
-#     """
-#     Get Biden's Tweets if file doesn't exist, if it does unpickle it
-#     """
-#     file = Path('BidenTweets.pickle')
-#     if file.is_file():
-#         input_file = open('BidenTweets.pickle', 'rb')
-#         reloaded_words = pickle.load(input_file)
-#         return reloaded_words
-#     else:
-#         get_tweets('@JoeBiden', 'BidenTweets.pickle')
-
-# BernieWords = get_bernie_tweets()
-# TrumpWords = get_trump_tweets()
-# BidenWords = get_biden_tweets()
+if __name__ == '__main__':
+    TrumpWords = get_user_tweets('@realDonaldTrump', 'TrumpTweets.pickle')
+    BernieWords = get_user_tweets('@BernieSanders', 'BernieTweets.pickle')
+    BidenWords = get_user_tweets('@JoeBiden', 'BidenTweets.pickle')
